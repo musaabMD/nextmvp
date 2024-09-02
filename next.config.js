8475d9e6dff820d2eba1fada7fb31b0616576e21
musaabMD <mousab.r@outlook.com>
2024-09-02 15:52:07 +0300

    “new”

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
       "pbs.twimg.com",
       "images.unsplash.com",
       "logos-world.net",
+      "m.media-amazon.com",
+      "static.tvtropes.org", // Added domain
     ],
   },
 };
